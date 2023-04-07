8b1728305e0348f657daf4f37d8a63ac1d2e4a43
Élise Rubio <elise.rubio@4sh.fr>
2023-04-07 09:39:18 +0200

    Add scripts to release

---
+++ release.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Stop on errors
+set -e
+
+display_usage() {
+  echo -e "Usage: release.sh <RELEASE_VERSION> <NEXT_DEV_VERSION>"
+}
+
+if [ $# -ne 2 ]
+then
+  display_usage
+  exit 1
+fi
+
+RELVERSION=$1
+DEVVERSION=$2
+
+pendingChangesInIndex=`git status -s --untracked-files=no | grep "^[^?]" | wc -l | tr -d '[[:space:]]'`
+if [ "$pendingChangesInIndex" != "0" ]
+then
+  # Stashing current changes
+  git stash
+fi
+
+./setVersion.sh $RELVERSION
+git add . && git commit -m "preparing $RELVERSION"
+
+mvn "-DreleaseVersion=$RELVERSION" "-DdevelopmentVersion=$DEVVERSION" -B release:prepare release:perform
+
+./setVersion.sh $DEVVERSION
+git add . && git commit -m "re-snapshoted restx.version property"
+
+if [ "$pendingChangesInIndex" != "0" ]
+then
+  # Stash pop should be made in the end only
+  git stash pop
+fi
+
+echo ""
+echo "  RESTX Release successful !"
---
+++ setVersion.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Stop on errors
+set -e
+
+display_usage() {
+  echo -e "Usage: setVersion.sh <VERSION>"
+}
+
+if [ $# -ne 1 ]
+then
+  display_usage
+  exit 1
+fi
+
+VERSION=$1
+
+sedi(){
+  case "`uname`" in
+      Darwin*) sed -i '' "$1" "$2" ;;
+      *) sed -i "$1" "$2" ;;
+  esac
+}
+
+sedi "s/<restx.version>[0-9\\.a-zA-Z\\-]*</<restx.version>$VERSION</g" pom.xml
+mvn versions:set -DnewVersion=$VERSION -DgenerateBackupPoms=false
